8aef6368ec6e20904d540dea9ce27734c74f48f7
claudio <111624820+A01710963@users.noreply.github.com>
2022-10-13 21:52:53 -0500

    Update CALCULADORA DE CALORIAS.py
    Programa final

--- CALCULADORA DE CALORIAS.py
+++ CALCULADORA DE CALORIAS.py
@@ -1,77 +1,109 @@
 # En este programa realizaremos una calculadora de calorias por dia
 # Claudio Gabriel Lopez Briceño
-def operacion (p, s, e, a):
+def operacion (peso, sexo, edad, actividad):
     """Lo que estoy haciendo aqui es la operacion necesaria para calcular las calorias diaras.
     La variable p se refiere a el peso, la variabel s al sexo, la variable e a la edad y la variable a a la actividad de la persona"""
-    cal = a*(p*(s*e))
-    return cal
-def menus ():
-    """Este menu le permitira al usuario elegir su sexo """
+    calorias = actividad*(peso*(sexo*edad))
+    return calorias
+def menuSexo ():
+    #Este menu le permitira al usuario elegir su sexo 
     print ("1. Masculino")
     print ("2. Femenino")
 
-def menua ():
-    """Este menu le permitira al usuario elegir su nivel de actividad"""
+def menuActividad ():
+    #Este menu le permitira al usuario elegir su nivel de actividad
     print ("1. No muy activo")
     print ("2. Poco activo")
     print ("3. Activo")
     print ("4. Muy activo")
     
-def menu ():
-     """Este menu es el menu general, le permitira al usuario elegir cuando salir del programa"""
+def menuPrincipal ():
+    #Este menu es el menu general, le permitira al usuario elegir cuando salir del programa
     print ("1. Calcular mi gasto calorico")
-    print ("2. Salir")
+    print ("2. Recomendaciones de comida")
+    print ("3. Salir")
+
+#Lista de recomendaciones de comida
+listaRecomendaciones = [
+    ["Proteinas:", "Carbohidratos:", "Grasas:", "Snacks:"],
+    ["Pollo", "Papa", "Nueces", "Fruta"],
+    ["Carnes rojas", "Pasta", "Aguacate", "Verdura"],
+    ["Huevo", "Arroz", "Aceite de oliva", "Palomitas de maiz"],
+    ["Salmon", "Pan integral", "Aceite de nabina", "Frutos secos"]
+    ]
+#Esta funcion imprime una columna de la matriz dependiendo de la eleccion del usuario
+def imprimirRecomendaciones (listaRecomendaciones):
+    i = input("¿De que deseas obtener recomendaciones? Proteinas(0) Carbohidratos(1) Grasas(2) Snacks(3): ")
+    columna = [fila[int(i)] for fila in listaRecomendaciones]
+    print (columna, end = "\n")
+    
+def pruebas():
+    operacion(18,17,16,5)
+    menuSexo()
+    menuActividad()
+    menuPrincipal()
+    imprimirRecomendaciones(listaRecomendaciones)
 
 def main ():
+    #pruebas()
     while(True):
-         """Aqui el usuario seleccionara su sexo"""
+            #Aqui el usuario seleccionara su sexo
             print ("Selecciona una opcion")
-            menu()
+            menuPrincipal()
             opcion = (input())
             if opcion == '1':
                 print ("¿Cual es tu sexo?")
-                menus()
-                opcions = (input())
+                menuSexo()
+                opcionSexo = (input())
                 while (True):
-                    if opcions == '1':
-                        s = 1
+                    #Dependiendo del sexo se cambia el numero, ya que asi lo pide la formula para calcular el gasto energetico
+                    if opcionSexo == '1':
+                        sexo = 1
                         break
                     elif opcions == '2':
-                        s = 0.9
+                        sexo = 0.9
                         break
                     else:
                         print ("Respuesta invalida, asegurate de haber insertado el numero correcto")
-                        menus()
+                        menuSexo()
                         opcions = (input())
-                 """Aqui el usuario seleccionara su nivel de actividad diaria"""
+                #Aqui el usuario seleccionara su nivel de actividad diaria
                 print ("¿Cual es tu nivel de actividad diaria?")
-                menua()
-                opciona = (input())
+                menuActividad()
+                opcionActividad = (input())
                 while (True):
-                    if opciona == '1':
-                        a = 1.2
+                    #Dependiendo del nivel de actividad se asigna un numero, ya que asi lo pide la formula para calcular el gasto energetico
+                    if opcionActividad == '1':
+                        actividad = 1.2
                         break
-                    elif opciona == '2':
-                        a = 1.375
+                    elif opcionActividad == '2':
+                        actividad = 1.375
                         break
-                    elif opciona == '3':
-                        a = 1.55
+                    elif opcionActividad == '3':
+                        actividad = 1.55
                         break
-                    elif opciona == '4':
-                        a = 1.725
+                    elif opcionActividad == '4':
+                        actividad = 1.725
                         break
                     else:
                         print ("Respuesta invalida, asegurate de haber insertado el numero correcto")
-                        menua()
+                        menuActividad()
                         opciona = (input())
-                """En estos dos apartados ingresara su peso y su edad"""
-                p = float(input("Ingresa tu peso en kilogramos: "))
+                #En estos dos apartados ingresara su peso y su edad
+                peso = float(input("Ingresa tu peso en kilogramos: "))
                 
-                e = int(input("Ingresa tu edad en años: "))
-                """Imprimimos la respuesta"""
-                print ("Tu consumo diario de calorias es de: ", operacion(p,s,e,a))
-                
+                edad = int(input("Ingresa tu edad en años: "))
+                #Imprimimos la respuesta
+                print ("Tu consumo diario de calorias es de: ", operacion(peso,sexo,edad,actividad))
+            
+            #En esta opcion imprimimos una columna de la matriz "listaRecomendaciones" dependiendo de la eleccion del usuario
             elif opcion == '2':
+                #Llamamos la funcion
+                imprimirRecomendaciones(listaRecomendaciones)
+            
+            #Esta opcion sirve para salir del programa
+            elif opcion == '3':
+                #Imprimos adios y terminamos el ciclo while
                 print ("Adios")
                 break
             else:
